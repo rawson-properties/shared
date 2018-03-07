ef9b990d524306f76526b9568170a5fbea1c4503
n1c <nicmalan@gmail.com>
2018-03-07 16:47:01 +0200

    Proper Intercom conf

--- src/config/intercom.php
+++ src/config/intercom.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
-    'app_id' => env('INTERCOM_APP_ID', 'n0fn42nw'),
+    'app_id' => env('INTERCOM_APP_ID', 'cerfjrpk'),
+    'api_key' => env('INTERCOM_API_KEY', '00d6a091181d70ada53741866a4b1c1757ee739a'),
     'secret' => env('INTERCOM_SECRET', 'gxFJsVcl7BsEW7KWQbgNJRAuF_WZh48xxRvZoGPX'),
 ];
