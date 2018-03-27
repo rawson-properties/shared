a100f7e4b967885883017e7f3bac488dc764b55e
n1c <nicmalan@gmail.com>
2018-03-27 15:36:57 +0200

    `config.intercom.access_token`

--- src/config/intercom.php
+++ src/config/intercom.php
@@ -2,6 +2,7 @@
 
 return [
     'app_id' => env('INTERCOM_APP_ID', 'cerfjrpk'),
+    'access_token' => env('INTERCOM_ACCESS_TOKEN', 'dG9rOmVmY2I0N2IyXzcyZGFfNDgyYV9iMTY0X2U3MWE1ODk2ZWEwMDoxOjA='),
     'api_key' => env('INTERCOM_API_KEY', '00d6a091181d70ada53741866a4b1c1757ee739a'),
     'secret' => env('INTERCOM_SECRET', 'gxFJsVcl7BsEW7KWQbgNJRAuF_WZh48xxRvZoGPX'),
 ];
