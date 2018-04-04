c41ead81e4c8bc8dff9799ac1832ad8efe25c8b8
n1c <nicmalan@gmail.com>
2018-04-04 11:32:55 +0200

    Logout route

--- src/routes/oauth.php
+++ src/routes/oauth.php
@@ -1,6 +1,7 @@
 <?php
 
 Route::get('login', [ 'as' => 'login', 'uses' => 'AuthController@login', ]);
+Route::post('logout', [ 'as' => 'logout', 'uses' => 'AuthController@logout', ]);
 
 Route::group([ 'prefix' => 'auth', 'as' => 'auth.', 'middleware' => 'guest', ], function () {
     Route::get('connect', [ 'as' => 'connect', 'uses' => 'AuthController@connect', ]);
