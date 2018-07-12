6b96657c6ce2fea429b7e54ea66adb6f1315b24c
n1c <nicmalan@gmail.com>
2018-07-12 10:33:48 +0200

    `IntercomEvent` job

---
+++ src/Jobs/IntercomEvent.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Rawson\Shared\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Intercom\IntercomClient;
+
+class IntercomEvent implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $email;
+    protected $eventName;
+    protected $meta;
+
+    public function __construct(string $email, string $eventName, array $meta = [])
+    {
+        $this->email = $email;
+        $this->eventName = $eventName;
+        $this->meta = $meta;
+    }
+
+    public function handle()
+    {
+        $client = new IntercomClient(config('intercom.access_token'), null);
+        $client->events->create([
+            'created_at' => time(),
+            'event_name' => $this->eventName,
+            'email' => $this->email,
+            'metadata' => $this->meta,
+        ]);
+    }
+}
