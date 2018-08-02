995d66852eb2e993bf0b976222374b8007b6e63c
n1c <nicmalan@gmail.com>
2018-08-02 14:26:46 +0200

    P24->sellerList

--- src/RT3Models/P24.php
+++ src/RT3Models/P24.php
@@ -5,4 +5,9 @@ namespace Rawson\Shared\RT3Models;
 class P24 extends Model
 {
     protected $table = 'p24';
+
+    public function sellerList()
+    {
+        return $this->belongsTo(SellerList::class, 'ID', 'P24ID');
+    }
 }
