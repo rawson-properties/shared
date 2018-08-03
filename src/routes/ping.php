f69f6e6f17752f9d29776f051552f47b24e1ac13
n1c <nicmalan@gmail.com>
2018-08-03 15:14:42 +0200

    Add a ping to the default database & rt3 database (if it exists) to the `/ping` endpoint.

--- src/routes/ping.php
+++ src/routes/ping.php
@@ -1,5 +1,11 @@
 <?php
 
 Route::get('ping', function () {
+    DB::connection(config('database.default'))->select('SELECT NOW()');
+
+    if (config('database.connections.rt3')) {
+        DB::connection('rt3')->select('SELECT NOW()');
+    }
+
     return response('pong');
 })->name('ping');
