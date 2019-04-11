87fec39a0becbf5cf3a70f8b845480e88ec58d7c
Nic Malan <nicmalan@gmail.com>
2019-04-11 09:41:55 +0200

    Fix Hubspot template key.
    The old one got deactivated because we let the hub expire :sob:

--- src/config/hubspot.php
+++ src/config/hubspot.php
@@ -3,6 +3,6 @@
 return [
     'portal_id' => env('HUBSPOT_PORTAL_ID', 2419433),
     'key' => env('HUBSPOT_KEY', '9334baed-ba48-4b71-9682-c906840d8df3'),
-    'template_key' => env('HUBSPOT_TEMPLATE_KEY', '9cc1f6ff-0cfc-4160-bb50-7c02a2e2950e'),
+    'template_key' => env('HUBSPOT_TEMPLATE_KEY', '4c8fe37d-2cef-4c3c-ba84-1c7c3c7c4d66'),
     'dev_key' => env('HUBSPOT_DEV_KEY', '66703093-04ba-463c-96f7-2b58c57e4c76'),
 ];
