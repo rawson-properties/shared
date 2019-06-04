5022e1bd13062bf2cc74ade7a2d9b739b817b909
Nic Malan <nicmalan@gmail.com>
2019-06-04 12:43:09 +0200

    Franchise::businessTypes relation

--- src/RT3Models/Franchise.php
+++ src/RT3Models/Franchise.php
@@ -6,6 +6,11 @@ class Franchise extends Model
 {
     protected $table = 'franchise';
 
+    public function businessTypes()
+    {
+        return $this->belongsToMany(BusinessType::class, 'franchisebusinesstype', 'FRANCHISEID', 'BUSINESSTYPEID');
+    }
+
     public function stakeholder()
     {
         return $this->belongsTo(Stakeholder::class, 'STAKEHOLDERID', 'ID');
