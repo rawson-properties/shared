b65d16d837c51530dabc87445a5e05a6f408ce45
Nic Malan <nicmalan@gmail.com>
2019-06-06 14:40:19 +0200

    `Franchise::getCountEmployeesAttribute`

--- src/RT3Models/Franchise.php
+++ src/RT3Models/Franchise.php
@@ -6,6 +6,17 @@ class Franchise extends Model
 {
     protected $table = 'franchise';
 
+    public function getCountEmployeesAttribute(): int
+    {
+        return Employee::join('agentlist', 'agentlist.EMPLOYEEID', 'employee.ID')
+            ->join('office', 'office.ID', 'agentlist.OFFICEID')
+            ->where('office.FRANCHISEID', $this->ID)
+            ->where('office.OFFICESTATUSID', OfficeStatus::ACTIVE)
+            ->where('agentlist.ACTIVE', 'y')
+            ->count()
+            ;
+    }
+
     public function businessTypes()
     {
         return $this->belongsToMany(BusinessType::class, 'franchisebusinesstype', 'FRANCHISEID', 'BUSINESSTYPEID');
