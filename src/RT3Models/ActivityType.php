50667ae82b0cf90f4f4c30f27f8b56b7289fb66d
Nic Malan <nic.malan@rawsonproperties.com>
2019-09-16 14:30:49 +0200

    Some more ActivityType constants

--- src/RT3Models/ActivityType.php
+++ src/RT3Models/ActivityType.php
@@ -7,5 +7,10 @@ class ActivityType extends Model
     protected $table = 'activitytype';
 
     // This list is incomplete!
-    const SHOWHOUSE = 41;
+    const SHOWHOUSE = 41; // Listing Show house
+    const LISTING_VIEWING = 43;
+    const PROPERTY_VIEWING = 53;
+    const PROPERTY_SHOWHOUSE = 55;
+    const CONTACT_VIEWING = 61;
+    const CONTACT_SHOWHOUSE = 63;
 }
