3539b6393eba9beebc23acb23df9a28eff78c650
Nic Malan <nic.malan@rawsonproperties.com>
2019-11-26 12:07:23 +0200

    Fire a `Account First Created` Appcues event for new accounts in @appcues helper

--- src/resources/views/appcues.blade.php
+++ src/resources/views/appcues.blade.php
@@ -11,6 +11,10 @@
                     'last_name': '{{ Auth::user()->default_agent->last_name }}',
                 @endif
             });
+
+            @if (Auth::user() && Auth::user()->created_at->gt(now()->subMinute()))
+                window.Appcues.track("{{ sprintf('%s Account First Created', config('app.name')) }}");
+            @endif
         </script>
     @endif
 @endauth
