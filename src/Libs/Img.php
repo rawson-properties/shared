38c23b253798f67293d4b2bdd54e253f48e58dc7
Nic Malan <nic.malan@rawsonproperties.com>
2020-01-30 17:03:39 +0200

    Img service lib

---
+++ src/Libs/Img.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Rawson\Shared\Libs;
+
+class Img
+{
+    public static function url(string $path, array $params = []): string
+    {
+        $query = http_build_query(array_merge($params, [
+            's' => self::makeSig($path, $params),
+        ]));
+
+        return sprintf('%s/%s?%s', config('img.base_url'), $path, $query);
+    }
+
+    private static function makeSig(string $path, array $params = []): string
+    {
+        unset($params['s']);
+        ksort($params);
+
+        return md5(sprintf(
+            '%s:%s?%s',
+            config('img.key'),
+            ltrim($path, '/'),
+            http_build_query($params),
+        ));
+    }
+}
