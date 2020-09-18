105781b56791ff497055955fbe95694fbe80523f
Nic Malan <nic.malan@rawsonproperties.com>
2020-09-18 12:14:07 +0200

    Remove Hubspot config

--- src/config/hubspot.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'portal_id' => env('HUBSPOT_PORTAL_ID', 2419433),
-    'key' => env('HUBSPOT_KEY', '9334baed-ba48-4b71-9682-c906840d8df3'),
-    'template_key' => env('HUBSPOT_TEMPLATE_KEY', '4c8fe37d-2cef-4c3c-ba84-1c7c3c7c4d66'),
-    'dev_key' => env('HUBSPOT_DEV_KEY', '66703093-04ba-463c-96f7-2b58c57e4c76'),
-];
