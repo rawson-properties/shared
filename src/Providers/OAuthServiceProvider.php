027cf05906db7b87475e659fe8b45ac2f63c21c6
Nic Malan <nic.malan@rawsonproperties.com>
2020-09-23 15:01:01 +0200

    Update map to boot in the Route service provider?

--- src/Providers/OAuthServiceProvider.php
+++ src/Providers/OAuthServiceProvider.php
@@ -5,17 +5,18 @@ namespace Rawson\Shared\Providers;
 use App;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Laravel\Socialite\SocialiteServiceProvider;
 
 class OAuthServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        App::register(\Laravel\Socialite\SocialiteServiceProvider::class);
+        App::register(SocialiteServiceProvider::class);
 
         $this->mergeConfigFrom(__DIR__ . '/../config/oauth.php', 'oauth');
     }
 
-    public function map()
+    public function boot()
     {
         Route::middleware('web')
             ->namespace('Rawson\Shared\Http\Controllers')
