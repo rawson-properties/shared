a7a4e341dcd180ceb20b094709ed825f4bc3511e
Nic Malan <nic.malan@rawsonproperties.com>
2020-09-23 12:35:57 +0200

    Whole stack of dependency updates for Laravel 8 and remove old style factories.

--- src/Providers/RawsonSharedServiceProvider.php
+++ src/Providers/RawsonSharedServiceProvider.php
@@ -23,10 +23,12 @@ class RawsonSharedServiceProvider extends ServiceProvider
 
     public function boot()
     {
+        /* This has all changed for Laravel 8
         if (App::environment('testing')) {
             $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
         }
 
         $this->app->make('Illuminate\Database\Eloquent\Factory')->load(__DIR__ . '/../database/factories');
+        */
     }
 }
