8fa3bc9fa973fc5c2833cb3fce60a245b6cdb5aa
Nic Malan <nic.malan@rawsonproperties.com>
2020-10-28 12:23:20 +0200

    EmployeeAward & AwardDescription RT3 models

--- src/RT3Models/Employee.php
+++ src/RT3Models/Employee.php
@@ -2,6 +2,9 @@
 
 namespace Rawson\Shared\RT3Models;
 
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+
 class Employee extends Model
 {
     protected $table = 'employee';
@@ -14,22 +17,34 @@ class Employee extends Model
         return $this->belongsTo(Person::class, 'PERSONID', 'ID');
     }
 
-    public function agents()
+    public function agents(): HasMany
     {
         return $this->hasMany(Agent::class, 'EMPLOYEEID', 'ID');
     }
 
-    public function courseHistory()
+    public function awards(): HasManyThrough
+    {
+        return $this->hasManyThrough(
+            AwardDescription::class,
+            EmployeeAward::class,
+            'EMPLOYEEID',
+            'ID',
+            'ID',
+            'AWARDDESCRIPTIONID'
+        );
+    }
+
+    public function courseHistory(): HasMany
     {
         return $this->hasMany(CourseHistory::class, 'EMPLOYEEID', 'ID');
     }
 
-    public function repSalesDetails()
+    public function repSalesDetails(): HasMany
     {
         return $this->hasMany(RepSalesDetail::class, 'employeeid', 'ID');
     }
 
-    public function repAchieverDetails2016()
+    public function repAchieverDetails2016(): HasMany
     {
         return $this->hasMany(RepAchieverDetail2016::class, 'employeeid', 'ID');
     }
