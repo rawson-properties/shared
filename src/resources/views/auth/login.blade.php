a07749a3237eccd4f6ddb5e8e9ee55e35dede9d2
Nic Malan <nic.malan@rawsonproperties.com>
2020-11-12 13:29:32 +0200

    Style tweaks

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -6,19 +6,19 @@
             <div class="col-12 col-sm-8 col-md-6 col-lg-4 col-xl-4 mt-5">
                 <div class="card p-4">
                     <div class="card-body text-center">
-                        <div class="row justify-content-center mb-4">
+                        <div class="row justify-content-center mb-5">
                             <div class="col-8">
                                 <img
                                     src="{{ asset('/images/rawson-tab-logo.png') }}"
-                                    class="img-fluid"
+                                    style="max-width: 150px;"
                                     title=""
                                 >
                             </div>
                         </div>
 
-                        <h5 class="font-weight-normal">Quick question before you log in</h4>
+                        <h5 class="font-weight-normal mb-4">Quick question before you log in</h4>
 
-                        <p class="card-text text-muted mb-5">
+                        <p class="card-text text-muted mb-5 text-justify">
                             Please confirm which region you're operating in so we can send you to the correct Google sign in page.
                         </p>
 
