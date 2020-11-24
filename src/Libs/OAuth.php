e1e819f8cd1eb05c34f9437a613388a050968277
Nic Malan <nic.malan@rawsonproperties.com>
2020-11-24 14:20:28 +0200

    Maybe?

--- src/Libs/OAuth.php
+++ src/Libs/OAuth.php
@@ -13,7 +13,8 @@ class OAuth
             throw new Exception('Invalid provider name!');
         }
 
-        $providerConfig['redirect'] = request()->getHost() . '/auth/callback';
+        $domain = parse_url(url()->current(), PHP_URL_HOST);
+        $providerConfig['redirect'] = sprintf('https://%s/auth/callback', $domain);
 
         // Push our config onto `services.google` for Socialite.
         config([
