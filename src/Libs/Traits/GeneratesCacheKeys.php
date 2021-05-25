be41aed88064df1715c06d30891457f4ae8d2df7
Nic Malan <nic.malan@rawsonproperties.com>
2021-05-25 10:56:41 +0200

    Add `$caller` to `GeneratesCacheKeys::key` method
    This puts in the calling function's name by default (we did this by habit already).

--- src/Libs/Traits/GeneratesCacheKeys.php
+++ src/Libs/Traits/GeneratesCacheKeys.php
@@ -6,8 +6,10 @@ trait GeneratesCacheKeys
 {
     public static function key(array $paramsSafe = [], array $paramsUnsafe = [])
     {
+        $caller = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2)[1]['function'];
+
         return implode('::', array_merge(
-            array_merge([ static::class ], $paramsSafe),
+            array_merge([ static::class, $caller, ], $paramsSafe),
             array_map('md5', $paramsUnsafe)
         ));
     }
