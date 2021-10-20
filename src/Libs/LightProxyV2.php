79a92d5f486b22c29da2622dc3fd9a2c52d5629d
Nic Malan <nic.malan@rawsonproperties.com>
2021-10-20 11:32:35 +0200

    Update `LightProxyV2` to support POST requests

--- src/Libs/LightProxyV2.php
+++ src/Libs/LightProxyV2.php
@@ -18,7 +18,7 @@ class LightProxyV2
         return rtrim(config('services.lightproxy-v2.url'), '/') . '/' . ltrim($url, '/');
     }
 
-    public static function get(string $url, array $params = [])
+    private static function request(string $method, string $url, array $params = [], array $payload = [])
     {
         if (!config('services.lightproxy-v2')) {
             throw new Exception('No LightProxyV2 config!');
@@ -26,22 +26,55 @@ class LightProxyV2
 
         $url = self::prepareUrl($url);
 
+        // @NOTE: This isn't safe if the url already has some params.
         if (count($params)) {
             $url .= '?' . http_build_query($params);
         }
 
-        $response = Http::withToken(config('services.lightproxy-v2.token'))->get($url);
-        $response->throw();
+        switch ($method) {
+            case 'get':
+                $response = Http::withToken(config('services.lightproxy-v2.token'))->get($url);
+                break;
+            case 'post':
+                $response = Http::withToken(config('services.lightproxy-v2.token'))->post($url, $payload);
+                break;
+            default:
+                throw new Exception('Not a valid request method!');
+        }
 
+        $response->throw();
         return $response->object();
+    }
+
+    public static function get(string $url, array $params = [])
+    {
+        return self::request('get', $url, $params);
+    }
+
+    public static function post(string $url, array $payload = [], array $params = [])
+    {
+        return self::request('post', $url, $params, $payload);
     }
 
     public static function getCached(string $url, array $params = [], CarbonInterval $interval = null)
     {
         $interval = $interval ?: CarbonInterval::day();
-        $key = self::key([ __FUNCTION__, ], array_merge([ 'url' => $url, ], $params));
+        $key = self::key([], array_merge([ 'url' => $url, ], $params));
         return Cache::remember($key, $interval, function () use ($url, $params) {
             return self::get($url, $params);
+        });
+    }
+
+    public static function postCached(
+        string $url,
+        array $payload = [],
+        array $params = [],
+        CarbonInterval $interval = null
+    ) {
+        $interval = $interval ?: CarbonInterval::day();
+        $key = self::key([], array_merge([ 'url' => $url, ], $payload, $params));
+        return Cache::remember($key, $interval, function () use ($url, $payload, $params) {
+            return self::post($url, $payload, $params);
         });
     }
 }
