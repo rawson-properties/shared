0b4050593491cfd2f398d7fea3e12163cc8682d0
Nic Malan <nicmalan@gmail.com>
2022-04-21 10:19:03 +0200

    Bump deps to PHP 8.1 & Laravel 9 (+ fix tests)

--- tests/Traits/GeneratesCacheKeysTest.php
+++ tests/Traits/GeneratesCacheKeysTest.php
@@ -12,30 +12,38 @@ class GeneratesCacheKeysTest extends TestCase
     public function testEmpty()
     {
         $result = self::key();
-        $this->assertEquals($result, self::class);
+        $this->assertEquals($result, sprintf('%s::testEmpty', self::class));
     }
 
     public function testBasic()
     {
-        $result = self::key([ 'testBasic', ]);
+        $result = self::key();
         $this->assertEquals($result, sprintf('%s::testBasic', self::class));
     }
 
     public function testBasicDouble()
     {
         $result = self::key([ 'testBasic', 'testDouble' ]);
-        $this->assertEquals($result, sprintf('%s::testBasic::testDouble', self::class));
+        $this->assertEquals($result, sprintf('%s::testBasicDouble::testBasic::testDouble', self::class));
     }
 
-    public function testUnsave()
+    public function testUnsafe()
     {
         $result = self::key([ 'testUnsafe', ], [ 'testUnsafe', ]);
-        $this->assertEquals($result, sprintf('%s::testUnsafe::%s', self::class, md5('testUnsafe')));
+        $this->assertEquals($result, sprintf('%s::testUnsafe::testUnsafe::%s', self::class, md5('testUnsafe')));
     }
 
     public function testUnsafeDouble()
     {
         $result = self::key([ 'testUnsafe', ], [ 'testUnsafe', 'testUnsafe', ]);
-        $this->assertEquals($result, sprintf('%s::testUnsafe::%s::%s', self::class, md5('testUnsafe'), md5('testUnsafe')));
+        $this->assertEquals(
+            $result,
+            sprintf(
+                '%s::testUnsafeDouble::testUnsafe::%s::%s',
+                self::class,
+                md5('testUnsafe'),
+                md5('testUnsafe'),
+            ),
+        );
     }
 }
