45aee1d08e84c8dc3b5aed264ca6d4894947afc2
Nic Malan <nicmalan@gmail.com>
2022-07-08 11:07:36 +0200

    Employee getIsPinnacleClubAttribute && getAchieverStatusAttribute

--- src/RT3Models/Employee.php
+++ src/RT3Models/Employee.php
@@ -2,13 +2,18 @@
 
 namespace Rawson\Shared\RT3Models;
 
+use Cache;
+use Carbon\Carbon;
+use Carbon\CarbonInterval;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Rawson\Shared\Database\Factories\EmployeeFactory;
+use Rawson\Shared\Libs\Traits\GeneratesCacheKeys;
 
 class Employee extends Model
 {
+    use GeneratesCacheKeys;
     use HasFactory;
 
     protected $table = 'employee';
@@ -19,6 +24,33 @@ class Employee extends Model
     protected static function newFactory()
     {
         return EmployeeFactory::new();
+    }
+
+    public function getIsPinnacleClubAttribute(): bool
+    {
+        $key = self::key([ __FUNCTION__, $this->ID, ]);
+        return Cache::remember($key, CarbonInterval::hours(2), function () {
+            $march = Carbon::parse('1st March this year');
+            $displayStartAt = now()->lt($march) ? $march->copy()->subYear() : $march;
+
+            $status = $this->awards()
+                ->whereIn('awarddescription.ID', [ 355, 357, ]) // Hardcoded IDs for Pinnacle Club awards
+                ->whereBetween('DATE', [
+                    $displayStartAt->copy()->startOfYear(),
+                    $displayStartAt->copy()->endOfYear(),
+                ])
+                ->count();
+
+            return $status > 0;
+        });
+    }
+
+    public function getAchieverStatusAttribute(): ?string
+    {
+        $achieverDetails = $this->repAchieverDetails2016()->first();
+        return $achieverDetails && $achieverDetails->currStatus !== 'No Status'
+            ? $achieverDetails->currStatus
+            : null;
     }
 
     public function person()
