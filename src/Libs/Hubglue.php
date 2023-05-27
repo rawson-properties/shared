62ff6c06b8052691ac0d633b4efc152ae9d02e5f
Elvis Magagula <kopahead@gmail.com>
2023-05-27 12:12:49 +0200

    fix: update

--- src/Libs/Hubglue.php
+++ src/Libs/Hubglue.php
@@ -2,8 +2,6 @@
 
 namespace Rawson\Shared\Libs;
 
-use Cache;
-use Carbon\CarbonInterval;
 use GuzzleHttp\Client as GuzzleClient;
 use Rawson\Shared\Libs\Traits\GeneratesCacheKeys;
 
@@ -22,31 +20,32 @@ class Hubglue
             config('services.hubglue.client_secret'),
         ]);
 
-        return Cache::remember($key, CarbonInterval::day(), function () {
-            $guzzle = new GuzzleClient();
-            $response = $guzzle->post(config('services.hubglue.url') . '/oauth/token', [
-                'form_params' => [
-                    'grant_type' => 'client_credentials',
-                    'client_id' => config('services.hubglue.client_id'),
-                    'client_secret' => config('services.hubglue.client_secret'),
-                ],
-            ]);
+        $guzzle = new GuzzleClient(['verify' => config('app.env') !== 'local']);
 
-            $json = json_decode((string) $response->getBody());
-            return object_get($json, 'access_token');
-        });
+        $response = $guzzle->post(config('services.hubglue.url').'/oauth/token', [
+            'form_params' => [
+                'grant_type' => 'client_credentials',
+                'client_id' => config('services.hubglue.client_id'),
+                'client_secret' => config('services.hubglue.client_secret'),
+            ],
+        ]);
+
+        $json = json_decode((string) $response->getBody());
+
+        return object_get($json, 'access_token');
     }
 
     protected static function get(string $endpoint, int $timeout = 5)
     {
-        $guzzle = new GuzzleClient();
+        $guzzle = new GuzzleClient(['verify' => config('app.env') !== 'local']);
+
         $response = $guzzle->request(
             'GET',
-            config('services.hubglue.url') . '/api/' . $endpoint,
+            config('services.hubglue.url').'/api/'.$endpoint,
             [
                 'headers' => [
                     'Accept' => 'application/json',
-                    'Authorization' => 'Bearer ' . self::getToken(),
+                    'Authorization' => 'Bearer '.self::getToken(),
                     'Content-Type' => 'application/json',
                 ],
                 'timeout' => $timeout,
@@ -58,15 +57,16 @@ class Hubglue
 
     private static function post(string $endpoint, array $data = [])
     {
-        $guzzle = new GuzzleClient();
+        $guzzle = new GuzzleClient(['verify' => config('app.env') !== 'local']);
+
         $response = $guzzle->request(
             'POST',
-            config('services.hubglue.url') . '/api/' . $endpoint,
+            config('services.hubglue.url').'/api/'.$endpoint,
             [
                 'json' => $data,
                 'headers' => [
                     'Accept' => 'application/json',
-                    'Authorization' => 'Bearer ' . self::getToken(),
+                    'Authorization' => 'Bearer '.self::getToken(),
                     'Content-Type' => 'application/json',
                 ],
             ]
