d902f5ca0704863a6845dd6426b6ba70990d8a21
Nic <nicmalan@gmail.com>
2024-06-06 10:07:29 +0200

    Update GeneratesCacheKeys to use App::environment and shorter md5 for params unsafe

--- src/Libs/Traits/GeneratesCacheKeys.php
+++ src/Libs/Traits/GeneratesCacheKeys.php
@@ -2,15 +2,19 @@
 
 namespace Rawson\Shared\Libs\Traits;
 
+use Illuminate\Support\Facades\App;
+
 trait GeneratesCacheKeys
 {
-    public static function key(array $paramsSafe = [], array $paramsUnsafe = [])
+    public static function key(array $paramsSafe = [], array $paramsUnsafe = []): string
     {
+        $env = App::environment();
         $caller = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2)[1]['function'];
 
         return implode('::', array_merge(
-            array_merge([ static::class, $caller, ], $paramsSafe),
-            array_map('md5', $paramsUnsafe)
+            [$env, static::class, $caller],
+            $paramsSafe,
+            [md5(implode('::', $paramsUnsafe))]
         ));
     }
 }
